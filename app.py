1cb2e2f9160a1246411268e7de411298c8734b81
karticr <cleverkriss@gmail.com>
2021-07-12 17:04:08 +0530

    flask init

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response, redirect, url_for, request, session, abort, render_template
+from threading import Thread
+
+from libs.shellController import ShellController
+from libs.configController import ConfigController
+
+from routes.home_page import home_page
+
+app = Flask(__name__)
+app.register_blueprint(home_page)
+
+@app.route('/yolo')
+def somehandler():
+    return "hello"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug = False) 
