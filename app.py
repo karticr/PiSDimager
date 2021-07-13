b6345f647234017a1a336169585c95008e0dd68c
karticr <cleverkriss@gmail.com>
2021-07-13 09:39:36 +0530

    still testing

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ from libs.configController import ConfigController
 
 from routes.home_page import home_page
 
+sc   = ShellController()
+conf = ConfigController()
+
+
 app = Flask(__name__)
 app.register_blueprint(home_page)
+
+app.conf = conf
+app.sc   = sc
+
 
 @app.route('/yolo')
 def somehandler():
     return "hello"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug = False) 
+    app.run(host='0.0.0.0', port=5000, debug = True) 
--- routes/home_page.py
+++ routes/home_page.py
@@ -1,8 +1,28 @@
 from flask import Blueprint, render_template, Response, redirect, url_for, request,jsonify
 from flask import current_app as app
-
+from threading import Thread
 home_page = Blueprint('home_page',__name__)
 
 @home_page.route('/')
 def home():
-    return "homepage"
+    sd_cards = app.sc.USBDeviceList()
+    conf     = app.conf.loadConfigFromFile()
+    
+    return render_template('home_page/home_page.html', conf=conf, sd_card_list=sd_cards)
+
+
+@home_page.route('/msgs')
+def msgs():
+    status = app.sc.status
+    prog   = app.sc.dd_prog_msg
+
+    data = {
+        "status": status,
+        "prog"  : prog
+    }
+    return jsonify(data)
+
+@home_page.route('/test')
+def testRun():
+    Thread(target=app.sc.ImageProcessor, args=('/dev/sdg', "test.img")).start()
+    return "okay"
